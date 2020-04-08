8652712baddf146cb0f1141ea2656aee0450a4c4
Mahamat Oumar <omar.mho20@gmail.com>
2020-04-08 01:08:19 -0400

    added backend implementation for donation

--- app/funder_views/index.py
+++ app/funder_views/index.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from app.models import Donor, Donation
 
 def index(request):
     context_dict = {}
+    donation = Donation.objects.filter()[0]
+    context_dict['campaign_name'] = donation.name
+    donors = Donor.objects.filter()
+    total = 0
+    for donor in donors:
+        total += donor.amount
+    donation.amount = total
+    donation.save()
+    context_dict['total'] = total
     return render(request, 'app/index.html', context_dict)
