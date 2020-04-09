512c847f9687bf4c2c7ba6be0588589ffe79e39b
David <dpantech@gmail.com>
2020-04-08 23:40:21 -0400

    Removed unused admin_urls.py file

--- fundrainsingapp/urls.py
+++ fundrainsingapp/urls.py
@@ -8,7 +8,6 @@ from app.funder_views.donate import donation
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('app.funder_urls')),
-    url(r'^adminstrators/', include('app.administrator_urls')),
-    
+
 
 ]
