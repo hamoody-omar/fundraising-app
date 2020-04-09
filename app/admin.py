40be0b8be8dd04da0751d3dee7721c3443606a01
JEGarcia_10 <JoshGarcia1995@live.com>
2020-04-08 22:27:03 -0400

    Added models, updated wording on home page

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Donation, Donor, DonorDonation
 
+admin.site.register(Donation)
+admin.site.register(Donor)
+admin.site.register(DonorDonation)
 # Register your models here.
