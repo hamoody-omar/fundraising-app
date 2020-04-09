10aabdb4a90895942c0aa7335ef6cc99396a3b48
David <dpantech@gmail.com>
2020-04-09 00:27:05 -0400

    Added sponsor frontend and backend

--- app/funder_views/sponsor.py
+++ app/funder_views/sponsor.py
@@ -7,25 +7,25 @@ def sponsor(request):
         try:
 
             if Sponsorship.objects.filter():
-                donation = Sponsorship.objects.filter()[0]
+                sponsorship = Sponsorship.objects.filter()[0]
             else:
-                donation = Sponsorship()
-                donation.name = "Corona virus fundraising"
-                donation.save()
+                sponsorship = Sponsorship()
+                sponsorship.name = "Corona virus sponsorship"
+                sponsorship.save()
 
-            donor = Sponsor()
-            donor.company_name = request.POST['firstname'] + " "+ request.POST['lastname']
-            donor.offer = request.POST['amount']
-            donor.business_email_address = request.POST['email']
-            donor.phone_number = request.POST['phonenumber']
-            donor.save()
+            sponsor = Sponsor()
+            sponsor.company_name = request.POST['firstname'] + " "+ request.POST['lastname']
+            sponsor.offer = request.POST['amount']
+            sponsor.business_email_address = request.POST['email']
+            sponsor.business_phone_number = request.POST['phonenumber']
+            sponsor.save()
 
-            donorDonation = DonorDonation()
-            donorDonation.donation = donation
-            donorDonation.donor = donor
+            sponsorSponsorship = SponsorSponsorship()
+            sponsorSponsorship.sponsorship = sponsorship
+            sponsorSponsorship.sponsor = sponsor
 
             return redirect('/')
         except:
-            render(request, 'app/donate.html', context_dict)
+            render(request, 'app/sponsor.html', context_dict)
     else:
-        return render(request, 'app/donate.html', context_dict)
+        return render(request, 'app/sponsor.html', context_dict)
--- fundrainsingapp/urls.py
+++ fundrainsingapp/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
 from app.funder_views.donate import donation
+from app.funder_views.sponsor import sponsor
 
 urlpatterns = [
     path('admin/', admin.site.urls),
