a0dd78e43a5ff299e4709f3fa295c29d3441d993
David <dpantech@gmail.com>
2020-04-12 23:15:45 -0400

    Functional Query Form but looks ugly

--- app/funder_views/queries.py
+++ app/funder_views/queries.py
@@ -5,18 +5,23 @@ from django.db.models import Sum
 def queries(request):
     context_dict = {}
     if request.POST:
-        try:
-            sponsors = Sponsor.object.all()
+        sponsors = Sponsor.objects.all()
 
-            reptype= request.POST['reptype']
-            sel_month= request.POST['month']
-            sel_quarter= request.POST['quart']
-            sel_year= request.POST['year']
+        reptype= int(request.POST['reptype'])
 
-            print(sponsors.filter(created_at__year=sel_year).aggregate(Yearly=Sum('offer')))
 
-            return redirect('/')
-        except:
-            return render(request, 'app/queries.html', context_dict)
+        
+
+        if reptype==1:
+            sel_month= int(request.POST['month'])
+            context_dict['result']=sponsors.filter(created_at__month=sel_month).aggregate(Monthly=Sum('offer'))
+        elif reptype==2:
+            sel_quarter= int(request.POST['quart'])
+            context_dict['result']=sponsors.filter(created_at__quarter=sel_quarter).aggregate(Quarterly=Sum('offer'))
+        elif reptype==3:
+            sel_year= int(request.POST['year'])
+            context_dict['result']=sponsors.filter(created_at__year=sel_year).aggregate(Yearly=Sum('offer'))
+
+        return render(request, 'app/queries.html', context_dict)
     else:
         return render(request, 'app/queries.html', context_dict)
